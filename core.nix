7c74300acf4516fbe670ad490b3b7624aa6acff3
Nick Cao <nickcao@nichi.co>
2023-02-10 16:03:55 +0800

    init

---
+++ core.nix
@@ -0,0 +1,6 @@
+{ fetchzip }:
+fetchzip {
+  url = "https://arch-archive.tuna.tsinghua.edu.cn/2023/02-06/core/os/x86_64/core.db.tar.gz";
+  stripRoot = false;
+  hash = "sha256-a9B0GC8/j3JM+eewBDuH0C/zxoGGEX8hyHRjGZvlbog=";
+}
---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs { inherit system; }; in {
+          packages.core = pkgs.callPackage ./core.nix { };
+        });
+}
