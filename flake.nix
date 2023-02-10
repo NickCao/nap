a854ff13898f99375a453b543db16378c3acd13b
Nick Cao <nickcao@nichi.co>
2023-02-10 17:07:40 +0800

    generalize repo handling

--- flake.nix
+++ flake.nix
@@ -7,10 +7,20 @@
     flake-utils.lib.eachDefaultSystem
       (system:
         let pkgs = import nixpkgs { inherit system; }; in {
-          packages.core = pkgs.callPackage ./core.nix {
+          packages.core = pkgs.callPackage ./archlinux.nix {
             repo = "core";
             date = "2023/02-06";
             hash = "sha256-RcVVXv56bmm6ZS2XBiXYBr4DqZFkCTBwc7F8JOIzsAM=";
+          };
+          packages.extra = pkgs.callPackage ./archlinux.nix {
+            repo = "extra";
+            date = "2023/02-06";
+            hash = "sha256-h2oYA85vMEkBiyCF1aU+NjP4dBhOGkeEGJrbXyWr2cA=";
+          };
+          packages.community = pkgs.callPackage ./archlinux.nix {
+            repo = "community";
+            date = "2023/02-06";
+            hash = "sha256-fiVQbySTYY9NeMC8eEXvJbquuFjvdE7Nzs0mcp74TGc=";
           };
         });
 }
