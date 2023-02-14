7d3581eb905e5b54bfef82fc7d206d3d0944614b
Nick Cao <nickcao@nichi.co>
2023-02-14 13:16:51 +0800

    rework debian packages handling

--- debian.nix
+++ debian.nix
@@ -1,13 +1,32 @@
-{ lib, releaseFile, fetchurl, linkFarm }:
+{ lib, releaseFile, fetchurl, runCommandNoCC, linkFarm, python3 }:
 let
   release = builtins.fromJSON (builtins.readFile releaseFile);
 in
 linkFarm release.distribution (builtins.map
-  (file: {
-    name = file.filename;
-    path = fetchurl {
+  (file:
+  let
+    packages = fetchurl {
       url = "${release.uri}/${file.filename}";
       sha256 = file.sha256;
     };
-  })
+    temp = runCommandNoCC "temp"
+      {
+        nativeBuildInputs = [
+          (python3.withPackages
+            (ps: with ps; [ debian ]))
+        ];
+      } ''
+      python ${./debian-packages} --input ${packages} --output $out
+    '';
+  in
+  [
+    {
+      name = file.filename;
+      path = packages;
+    }
+    {
+      name = "${file.filename}.temp";
+      path = temp;
+    }
+  ])
   release.files)
