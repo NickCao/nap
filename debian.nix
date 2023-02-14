9e8e802e1661c60d791380167e9f6f4c0e9534e6
Nick Cao <nickcao@nichi.co>
2023-02-14 13:56:01 +0800

    save debian release into sqlite database

--- debian.nix
+++ debian.nix
@@ -1,32 +1,19 @@
 { lib, releaseFile, fetchurl, runCommandNoCC, linkFarm, python3 }:
 let
   release = builtins.fromJSON (builtins.readFile releaseFile);
-in
-linkFarm release.distribution (builtins.map
-  (file:
-  let
-    packages = fetchurl {
+  packages = builtins.map
+    (file: fetchurl {
       url = "${release.uri}/${file.filename}";
       sha256 = file.sha256;
-    };
-    temp = runCommandNoCC "temp"
-      {
-        nativeBuildInputs = [
-          (python3.withPackages
-            (ps: with ps; [ debian ]))
-        ];
-      } ''
-      python ${./debian-packages} --input ${packages} --output $out
-    '';
-  in
-  [
-    {
-      name = file.filename;
-      path = packages;
-    }
-    {
-      name = "${file.filename}.temp";
-      path = temp;
-    }
-  ])
-  release.files)
+    })
+    release.files;
+in
+runCommandNoCC "${release.distribution}.db"
+{
+  nativeBuildInputs = [
+    (python3.withPackages
+      (ps: with ps; [ debian ]))
+  ];
+} ''
+  python ${./debian-packages} ${lib.escapeShellArgs packages} --output $out
+''
